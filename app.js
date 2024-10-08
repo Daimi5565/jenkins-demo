c300ef33f66d9859ffd288573fd03b5a4b2e5c18
root <root@jenkins.opsoura.com>
2024-10-08 17:26:34 +0200

    Fix vulnerabilities and update dependencies

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 8081;  // Change port to 8081
+const port = 8081;  // Changed port to 8081
 
 app.get('/', (req, res) => {
   res.send('Hello, Jenkins! Running on port 8081');
 });
 
-// Change this line:
+// Changed this line:
 app.listen(port, '0.0.0.0', () => {
   console.log(`App listening at http://0.0.0.0:${port}`);
 });
